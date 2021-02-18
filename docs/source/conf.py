9d001825fb33e3162bc7bf347ee1e76df60a89ab
Grzegorz Miejski <g.miejski@smartrecruiters.com>
2021-02-18 15:53:20 +0100

    readthedocs-sphinx-search

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 extensions = [
     "sphinx_rtd_theme",
     'recommonmark',
-    "readthedocs-sphinx-search"
+    'sphinx_search.extension',
 ]
 
 html_theme = "sphinx_rtd_theme"
